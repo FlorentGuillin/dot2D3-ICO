f6a8dc9051b683b0d10bb6121e117699e40cdd58
Florent Guillin <florent.guillin@gmail.com>
2014-12-23 14:13:35 +0100

    Syntaxe ajoutée, graph correct pour a.dot, modif's nécessaires pour les autres

---
+++ syntax.mli
@@ -0,0 +1,13 @@
+type id = ID of string
+
+and edgeRHS = EDGERHS_E
+	|	EDGERHS_N of (id * id) * edgeRHS
+	| EDGERHS_S of stmt * edgeRHS
+
+and stmt = NODE_STMT of (id * id) * ((id * id) list) list
+	| EDGE_STMT of stmt * edgeRHS * ((id * id) list) list
+	| ATTR_STMT of string * ((id * id) list) list 
+	| ID_ID of (id * id) 
+	| SUBGRAPH of id * stmt list
+
+and graph = GRAPH of id * stmt list;;
